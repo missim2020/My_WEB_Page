ff4ac78669b9dc6ffdd7c2b36fe9784ffc9554c4
Simona <62394338+missim2020@users.noreply.github.com>
2020-12-17 16:10:51 +0000

    Image carousel: change pictures, add description of every photo, except the first. description changes automatically.

--- JS/app.js
+++ JS/app.js
@@ -69,16 +69,33 @@ const prev = document.getElementById("btn-prev"),
       next = document.getElementById("btn-next"),
       slides = document.querySelectorAll(".slide"), // we're getting all slides in one go
       dots = document.querySelectorAll(".dot"),
-      pause=document.getElementById("pause");  
+      pause=document.getElementById("pause"),
+      descriptionText = document.getElementById("description"),
+      textHolder=document.getElementById("text-bar");
 
 let index = 0;
-
+let description = [
+"Random facts about me", 
+"I like baking cakes more than eating them",
+"I used to ice skate across a frozen lake to go to school in the winter", 
+"I grew up in a small village with no running hot water and only an outdoor W.C.", 
+"I know how to milk a cow", 
+"After I finished school I was accepted by a military school, but decided to go elsewhere",
+"At school I was removed from the choir after the teacher found out how bad my singing voice was", 
+"My favourite movie is '3 idiots'"
+]
 
 const activeSlide = n => {
 for (slide of slides){
   slide.classList.remove("active");
 }
 slides[n].classList.add("active");
+descriptionText.innerHTML = description[index];
+if (index>0){
+  textHolder.style.display="block"
+}else{
+  textHolder.style.display="none"
+}
 }
 
 const activeDot = n => {
@@ -127,12 +144,12 @@ item.addEventListener("click",()=>{
 })
 
 let playing=true;
-let interval = setInterval(nextSlide, 5000);
+let interval = setInterval(nextSlide, 6000);
 
 function playSlideshow() {
   pause.innerHTML = 'Pause';
   playing = true;
-  interval = setInterval(nextSlide,5000);
+  interval = setInterval(nextSlide,6000);
 }
 function pauseSlideshow() {
   pause.innerHTML = 'Play';
